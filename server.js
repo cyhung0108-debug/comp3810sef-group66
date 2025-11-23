893ff512b6a31dde45b302d6796802f6dc79bd8a
cyhung0108-debug <cyhung0108@gmail.com>
2025-11-24 01:34:53 +0800

    Update server.js

--- server.js
+++ server.js
@@ -61,4 +61,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(http://localhost:${PORT}));
-app.get('/', (req, res) => res.redirect('/posts'));s
+app.get('/', (req, res) => res.redirect('/posts'));
+
